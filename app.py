853adf8f47a9ed149b4d557751db01ff71183048
Nikolay Golub <nikolay.v.golub@gmail.com>
2016-10-06 19:45:22 +0300

    Add native websocket example

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from constants import HTTP_WS_URL
 
 app = Flask(__name__)
 app.config['WS_SERVER'] = 'ws://echo.websocket.org'
-app.config['HTTP_WS_SERVER'] = 'http://127.0.0.1{url}'.format(url=HTTP_WS_URL)
+WS_ENDPOINT = '127.0.0.1{url}'.format(url=HTTP_WS_URL)
+app.config['HTTP_WS_SERVER'] = 'http://' + WS_ENDPOINT
+app.config['WS_SERVER'] = 'ws://' + WS_ENDPOINT
 
 
 LIBRARIES = (
     ('sockjs-client', 'sockjs-client'),
     ('Socket.io', 'Socket.io'),
-    ('socket-cluster', 'socket cluster')
+    ('socket-cluster', 'socket cluster'),
+    ('HTML5', 'HTML5 (native)')
 )
 
 
